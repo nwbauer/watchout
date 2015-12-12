fbd8f24dda048dccd3318d8fa30c61a2cca63033
nwbauer <nicolaswilliam.bauer@gmail.com>
2015-12-12 12:27:17 -0800

    in design phase

--- client/watchout.js
+++ client/watchout.js
@@ -3,7 +3,11 @@
 // start slingin' some d3 here.
 
 //game settings
+var currentLevel = 1;
+var levelTime = 15*1000;
 
+var longestTime = 0;
+var timeIncrement = 200;
 var up = 87;
 var down = 83;
 var left = 65;
@@ -36,34 +40,53 @@ var planet;
 var solarSytem = [];
 
 
-var colors = ['yellow', 'red', 'blue', 'green', 'purple'];
+//addEnemies();
 
-for (var i = 0; i < colors.length; i++) {
-  planet = {};
-  planet.color = colors[i];
-  planet.x = random(100);
-  planet.y = random(100);
-  planet.r = random(10);
-  planet.distance = distanceFunc(planet.x, heroData.x, planet.y, heroData.y) - (planet.r + heroData.r);
+var firstClicked = true;
+var time;
 
-solarSytem.push(planet);
-};
-
-
-var pieces = gameboard.data(solarSytem).enter().append('circle');
-pieces.transition().duration(1000).attr('cx', function(d){ return d.x; }).attr('cy', function(d){ return d.y; }).attr('r', function(d){ return d.r; }).style('fill', function(d){ return d.color;});
-console.log(gameboard[0]);
 
 d3.select('svg').on('click', function(d) {
-  //console.log(this);
+  time = 0;
   gameRunning = true;
+  health = 100;
+  hero.style('fill', 'orange');
+  d3.select('.collisions > span').text(health);
+  d3.select('.healthIndicator').style('width', health + '%');
 
-  setInterval(function(){
-    if(gameRunning){
-      d3.selectAll('circle:not(.hero)').transition().tween('circle', function() {return checkingContact.bind(this);}).ease(d3.ease('linear')).duration(1000).attr('cx', function (d) { d.x = random(100); return d.x; }).attr('cy', function (d) { d.y = random(100); return d.y; });
-    };
-  }, 2000);
+  if(firstClicked){
+    firstClicked = false;
+    setInterval(function() {
+      if(gameRunning){
+        time += timeIncrement;
+
+        if(time > (currentLevel-1)*levelTime){
+          currentLevel++;
+          d3.select(".level > span").text(currentLevel);
+          console.log("added 5");
+          addEnemies();
+        }
+
+        d3.select('.time > span').text(time/1000 + " sec");
+      }
+    }, timeIncrement)
+    //console.log(this);
+
+    setInterval(function(){
+      if(gameRunning){
+        d3.selectAll('circle:not(.hero)').transition().tween('circle', function() {return checkingContact.bind(this);}).ease(d3.ease('linear')).duration(1000).attr('cx', function (d) { d.x = random(150); return d.x; }).attr('cy', function (d) { d.y = random(150); return d.y; });
+      };
+    }, 2000);
+  }else{
+    removeAllEnemies();
+    addEnemies();
+  }
+
+
+
 });
+
+
 
 d3.select('body').on('keyup', function (){
   keypressed = d3.event.which;
@@ -71,17 +94,19 @@ d3.select('body').on('keyup', function (){
   //alert('keycode');
   var hero = d3.select('.hero');
   
-  if(keypressed === up){
-    hero.transition().duration(100).attr('cy', function(d){ d.y -= movement; return d.y});
-  }else if(keypressed === down){
-    hero.transition().duration(100).attr('cy', function(d){ d.y += movement; return d.y});
-  }else if(keypressed === left){
-    hero.transition().duration(100).attr('cx', function(d){ d.x -= movement; return d.x });
-  }else if(keypressed === right){
-    hero.transition().duration(100).attr('cx', function(d){ d.x += movement; return d.x });
-  } else if (keypressed === 32){
-    d3.selectAll('circle:not(.hero)').each(function(d){ console.log(checkingContact.call(this,d,true))});
-    debugger;
+  if(gameRunning){
+    if(keypressed === up){
+      hero.transition().duration(100).attr('cy', function(d){ d.y -= movement; return d.y});
+    }else if(keypressed === down){
+      hero.transition().duration(100).attr('cy', function(d){ d.y += movement; return d.y});
+    }else if(keypressed === left){
+      hero.transition().duration(100).attr('cx', function(d){ d.x -= movement; return d.x });
+    }else if(keypressed === right){
+      hero.transition().duration(100).attr('cx', function(d){ d.x += movement; return d.x });
+    } else if (keypressed === 32){
+      d3.selectAll('circle:not(.hero)').each(function(d){ console.log(checkingContact.call(this,d,true))});
+      debugger;
+    }
   }
 })
 
@@ -117,18 +142,51 @@ var r2 = heroData.r;
   if(collisionGFunc(x1, x2, y1, y2, r1, r2) && !inCollision){
     //debugger
     collisions++;
-    if(collisions%10){ health--;};
+    if(collisions%10){ 
+      health--;
+      d3.select('.collisions > span').text(health);
+      d3.select('.healthIndicator').style('width', health + '%');
+    };
 
-    
-    d3.select('.collisions > span').text(health);
     if(health <= 0 && gameRunning){
-      hero.style('fill', 'black')
+      d3.selectAll('circle').transition().duration(0);
+      hero.style('fill', 'black');
       gameRunning = false;
+
+      if(time > longestTime){
+        longestTime = time;
+        d3.select('.highscore > span').text(longestTime/1000 + ' sec')
+      }
+
       alert('GAME OVER');
     }
 
   }
 };
 
+function addEnemies(number) {
+  var colors = ['yellow', 'red', 'blue', 'green', 'purple'];
+
+  var newEnemyProperties = [];
+  for (var i = 0; i < colors.length; i++) {
+    planet = {};
+    planet.color = colors[i];
+    planet.x = random(100);
+    planet.y = random(100);
+    planet.r = random(10);
+    planet.distance = distanceFunc(planet.x, heroData.x, planet.y, heroData.y) - (planet.r + heroData.r);
+
+    solarSytem.push(planet);
+    newEnemyProperties.push(planet);
+  };
+
+  var pieces = gameboard.data(newEnemyProperties).enter().append('circle');
+  pieces.transition().duration(1000).attr('cx', function(d){ return d.x; }).attr('cy', function(d){ return d.y; }).attr('r', function(d){ return d.r; }).style('fill', function(d){ return d.color;});
+}
+
+function removeAllEnemies(number){
+  console.log("restart");
+  d3.selectAll('circle:not(.hero)').remove();
+}
 
 
